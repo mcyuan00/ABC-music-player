e9331019907921eaabfa85309e3e8588756a7bd3
Jennifer <jjslee@mit.edu>
2015-12-02 16:41:33 -0500

    added abstraction function, rep invariant, rep exposure safety, and rest constructor to playerelement

--- src/abc/player/PlayerElement.java
+++ src/abc/player/PlayerElement.java
@@ -1,16 +1,29 @@
 package abc.player;
 
 /**
- * Player element contains all the information needed to add a note
+ * PlayerElement contains all the information needed to add a note
  * in the sequence player, which are: pitch, startTick, and numTicks.
- * This class is immutable.
- *
  */
 public class PlayerElement {
+    
+    /**
+     * Abstraction function:
+     * PlayerElement represents a musical element that can be played
+     * by a sequence player.
+     * 
+     * Rep invariant:
+     * startTick must be the 0th tick or after (>=0)
+     * numTicks must be greater than 0 (>0)
+     * 
+     * Safety from rep exposure:
+     * This class is immutable.
+     * All variables are private and final.
+     */
     
     private final Pitch pitch;
     private final int startTick;
     private final int numTicks;
+    private final boolean isRest;
     
     /**
      * Represents a note that can be added to the SequencePlayer
@@ -22,6 +35,20 @@ public class PlayerElement {
         this.pitch = pitch;
         this.startTick = startTick;
         this.numTicks = numTicks;
+        this.isRest = false;
+        checkRep();
+    }
+    
+    /**
+     * Represents a rest that can be added to the SequencePlayer
+     * @param startTick the start tick the rest is to be played at
+     * @param numTicks the number of ticks the rest will be played for
+     */
+    public PlayerElement(int startTick, int numTicks){
+        this.pitch = new Pitch('c');
+        this.startTick = startTick;
+        this.numTicks = numTicks;
+        this.isRest = true;
         checkRep();
     }
     
@@ -35,7 +62,7 @@ public class PlayerElement {
     
     /**
      * 
-     * @return the start tick the note is to be played at
+     * @return the start tick the note or rest is to be played at
      */
     public int startTick(){
         return startTick;
@@ -43,12 +70,15 @@ public class PlayerElement {
     
     /**
      * 
-     * @return the number of ticks the note will be played for
+     * @return the number of ticks the note or rest will be played for
      */
     public int numTicks(){
         return numTicks;
     }
     
+    /**
+     * Asserts rep invariant.
+     */
     private void checkRep(){
         assert startTick >= 0;
         assert numTicks > 0;
