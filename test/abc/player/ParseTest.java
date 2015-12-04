452ce763d4db919ae3df4cb110cd93475a657765
Jennifer <jjslee@mit.edu>
2015-12-04 14:45:00 -0500

    changed parsetest so that window stays open until we close it

--- test/abc/player/ParseTest.java
+++ test/abc/player/ParseTest.java
@@ -1,10 +1,18 @@
 package abc.player;
 
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import javax.print.PrintException;
+import javax.swing.JDialog;
+
 import org.antlr.v4.gui.Trees;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.Utils;
 import org.antlr.v4.runtime.tree.ParseTree;
-
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.Test;
 
 import abc.parser.HeaderLexer;
@@ -16,19 +24,24 @@ public class ParseTest {
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
+
     //test header parse
-    
+
     @Test
     public void testHeaderParser(){
-        CharStream stream = new ANTLRInputStream("X:1\nT:h\nK:A\n");
+        CharStream stream = new ANTLRInputStream("X:1\nT:hello world\nK:A\n");
         HeaderLexer lexer = new HeaderLexer(stream);
         TokenStream tokens = new CommonTokenStream(lexer);
-        
+
         HeaderParser parser = new HeaderParser(tokens);
         ParseTree tree = parser.root();
-        Trees.inspect(tree, parser);
+        Future<JDialog> inspect = Trees.inspect(tree, parser);
+        try {
+            Utils.waitForClose(inspect.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
+
     //test body parse
 }
