d079b70f4e0dfa3667db426e35eabe7ebea65869
Jennifer <jjslee@mit.edu>
2015-12-05 03:39:37 -0500

    added testing partitions for parsetest

--- test/abc/player/ParseTest.java
+++ test/abc/player/ParseTest.java
@@ -25,8 +25,43 @@ public class ParseTest {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
 
-    //test header parse
-
+    
+    
+    // HeaderParser
+    // Testing partitions:
+   
+    // Valid index (first field and digit(s))
+    // Invalid index (missing X:, not first field, not digit(s))
+    
+    // Valid title (second field any text)
+    // Invalid title (missing T:, not second field)
+    
+    // Valid composer (any text)
+    // Invalid composer (missing C:)
+    
+    // Valid meter (digit(s)/digit(s) or C or C|)
+    // Invalid meter (missing M:, invalid characters)
+    
+    // Valid length (digit(s)/digit(s))
+    // Invalid length (missing L:, invalid characters)
+    
+    // Valid tempo (digit(s)/digit(s) = digit(s))
+    // Invalid tempo (missing T:, invalid characters)
+    
+    // Valid voice (any text)
+    // Invalid voice (missing V:)
+    
+    // Valid key (last field and case-insensitive note + optional accidental + optional minor)
+    // Invalid key (missing K:, not last field, note does not exist, accidental does not exist)
+    
+    // Valid comment (% followed by any text, not treated as a field)
+    // Invalid comment (missing %, treated as separate field)
+    
+    // Valid whitespace (all whitespace should be ignored/skipped)
+    
+    // Invalid field (no newline at end of field)
+    
+    // covers valid index, valid title, valid key
     @Test
     public void testHeaderParser(){
         CharStream stream = new ANTLRInputStream("X:1\nT:hello world\nK:A\n");
