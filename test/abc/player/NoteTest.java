7933d93aec9fe3817732d70948a6b237f24d40fe
Joseph Park <psy01212@mit.edu>
2015-12-07 02:04:47 -0500

    wrote test for getAllDurations() for Note

--- test/abc/player/NoteTest.java
+++ test/abc/player/NoteTest.java
@@ -17,12 +17,16 @@ public class NoteTest {
     //testing strategy:
     //  test both constructors
     //  test observers: pitch(), duration(), getPlayerElement()
+    //  test getAllDuratinos()
     
     @Test
     public void testNoteNoAccidentalMiddleOctave(){
         Note note = new Note(new Fraction(3, 4), 'B', 0);
         assertEquals(new Pitch('B'), note.pitch());
         assertEquals(new Fraction(3, 4), note.duration());
+        List<Fraction> allDurations = new ArrayList<>();
+        allDurations.add(new Fraction(3, 4));
+        assertEquals(allDurations, note.getAllDurations());
         List<PlayerElement> playerElements = new ArrayList<>();
         playerElements.add(new PlayerElement(new Pitch('B'), 5, 11));
         assertEquals(playerElements, note.getPlayerElements(5, 2, new Fraction(1, 4)));
@@ -33,6 +37,9 @@ public class NoteTest {
         Note note = new Note(new Fraction(5, 8), 'C', 1);
         assertEquals(new Pitch('C').transpose(Pitch.OCTAVE), note.pitch());
         assertEquals(new Fraction(5, 8), note.duration());
+        List<Fraction> allDurations = new ArrayList<>();
+        allDurations.add(new Fraction(5, 8));
+        assertEquals(allDurations, note.getAllDurations());
         List<PlayerElement> playerElements = new ArrayList<>();
         playerElements.add(new PlayerElement(new Pitch('C').transpose(Pitch.OCTAVE), 0, 5));
         assertEquals(playerElements, note.getPlayerElements(0, 1, new Fraction(1, 8)));
@@ -43,6 +50,9 @@ public class NoteTest {
         Note note = new Note(new Fraction(2, 8), 'G', -2, -1);
         assertEquals(new Pitch('G').transpose(Pitch.OCTAVE * -2 - 1), note.pitch());
         assertEquals(new Fraction(2, 8), note.duration());
+        List<Fraction> allDurations = new ArrayList<>();
+        allDurations.add(new Fraction(2, 8));
+        assertEquals(allDurations, note.getAllDurations());
         List<PlayerElement> playerElements = new ArrayList<>();
         playerElements.add(new PlayerElement(new Pitch('G').transpose(Pitch.OCTAVE * -2 - 1), 12, 16));
         assertEquals(playerElements, note.getPlayerElements(12, 16, new Fraction(1, 1)));
