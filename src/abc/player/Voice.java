234249590f16eaefe4873fdadad30cca09c57149
Joseph Park <psy01212@mit.edu>
2015-12-07 13:42:55 -0500

    changed Voice to take a list of Music instead of Measure

--- src/abc/player/Voice.java
+++ src/abc/player/Voice.java
@@ -9,23 +9,23 @@ import java.util.List;
  */
 public class Voice implements Music {
     private final String name;
-    private final List<Measure> measures;
+    private final List<Music> musicElements;
     
     /**
      * Make a Voice with a given name identifier and measures.
      * @param name identifies the voice
-     * @param measures music for the voice to play
+     * @param musicElements music for the voice to play
      */
-    public Voice(String name, List<Measure> measures){
+    public Voice(String name, List<Music> musicElements){
         this.name = name;
-        this.measures = measures;
+        this.musicElements = musicElements;
     }
 
     @Override
     public Fraction duration() {
         List<Fraction> durations = new ArrayList<Fraction>();
-        for (Measure measure : measures){
-            durations.add(measure.duration());
+        for (Music music : musicElements){
+            durations.add(music.duration());
         }
         return Fraction.sumAllFractions(durations);
     }
@@ -42,19 +42,19 @@ public class Voice implements Music {
      * Returns the measures that the voice plays
      * @return ArrayList<Measure> of measures played by the voice
      */
-    public List<Measure> measures(){
-        return new ArrayList<Measure>(measures);
+    public List<Music> measures(){
+        return new ArrayList<Music>(musicElements);
     }
 
     @Override
     public List<PlayerElement> getPlayerElements(int startTick, int ticksPerBeat, Fraction pieceNoteLength) {
         List<PlayerElement> elements = new ArrayList<PlayerElement>();
         int currentStart = startTick;
-        for (Measure measure : measures){
-            elements.addAll(measure.getPlayerElements(currentStart, ticksPerBeat, pieceNoteLength));
+        for (Music music : musicElements){
+            elements.addAll(music.getPlayerElements(currentStart, ticksPerBeat, pieceNoteLength));
             
             // calculate the start tick of the next music element
-            Fraction numBeats = new Fraction(measure.duration().numerator()*pieceNoteLength.denominator(), measure.duration().denominator()*pieceNoteLength.numerator()).simplify();
+            Fraction numBeats = new Fraction(music.duration().numerator()*pieceNoteLength.denominator(), music.duration().denominator()*pieceNoteLength.numerator()).simplify();
             int musicDuration = ticksPerBeat*numBeats.numerator()/numBeats.denominator();
             currentStart = currentStart + musicDuration;
         }
@@ -64,7 +64,7 @@ public class Voice implements Music {
     @Override
     public List<Fraction> getAllDurations() {
         List<Fraction> allDurations = new ArrayList<>();
-        for (Music music : measures){
+        for (Music music : musicElements){
             allDurations.addAll(music.getAllDurations());
         }
         return allDurations;
